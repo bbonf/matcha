8d23582dfe377401afe958a5aac8231cb464f161
Ben Bonfil <bonfil@gmail.com>
2016-03-19 17:32:33 +0100

    removed bootstraping code

--- matcha/java/bootstrap.java
+++
@@ -1,8 +0,0 @@
-private static class sys {
-    public static void log(Object... msgs) {
-        System.out.println(String.join(" ",
-            Arrays.asList(msgs)
-            .stream().map(x -> { return x.toString(); })
-            .collect(Collectors.toList())));
-    }
-}
